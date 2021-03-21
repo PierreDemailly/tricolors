f28ef366a64addcc6beaa1804e233735ce9d3d0f
PierreDemailly <pierredemailly.pro@gmail.com>
2021-03-22 00:37:02 +0100

    remove color after string

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const tricolors = {
   red(msg) {
-    return "\033[0;31m" + msg;
+    return "\033[0;31m" + msg + "\033[0m";
   },
   green(msg) {
-    return "\033[0;32m" + msg;
+    return "\033[0;32m" + msg + "\033[0m";
   },
   blue(msg) {
-    return "\033[0;34m" + msg;
+    return "\033[0;34m" + msg + "\033[0m";
   },
   redLog(msg) {
     console.log(red(msg));
