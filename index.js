4a9ba3cd7e006b1985259077d1ea43546b44d821
PierreDemailly <pierredemailly.pro@gmail.com>
2021-03-21 19:45:42 +0100

    remove log

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const tricolors = {
   red(msg) {
-    console.log("\033[0;31m" + msg);
+    return "\033[0;31m" + msg;
   },
   green(msg) {
-    console.log("\033[0;32m" + msg);
+    return "\033[0;32m" + msg;
   },
   blue(msg) {
-    console.log("\033[0;34m" + msg);
-  },
+    return "\033[0;34m" + msg;
+  }
 };
 
 module.exports = tricolors;
