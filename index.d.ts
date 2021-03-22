04a70def852cae21231dbc24aa0fd359654931b6
PierreDemailly <pierredemailly.pro@gmail.com>
2021-03-22 17:11:55 +0100

    declare module

--- index.d.ts
+++ index.d.ts
@@ -1,10 +1,9 @@
-import tricolors from ".";
-interface itricolors {
-  red(msg: string): string;
-  green(msg: string): string;
-  blue(msg: string): string;
-  redLog(msg: string): void;
-  greenLog(msg: string): void;
-  blueLog(msg: string): void;
+// <reference types="node" />
+declare module "tricolors" {
+  function red(msg: string): string;
+  function green(msg: string): string;
+  function blue(msg: string): string;
+  function redLog(msg: string): void;
+  function greenLog(msg: string): void;
+  function blueLog(msg: string): void;
 }
-exports.module = <itricolors>tricolors;
